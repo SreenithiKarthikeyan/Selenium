d945c185a79f7b2873860f6bca5a81f7872a7150
SreenithiKarthikeyan <KARTHIKEYANSREENITHI@GMAIL.COM>
2025-07-24 17:30:12 +0530

    testing changes done

---
+++ Selenium_EdgeBrowser_Launch/src/com/EdgeBrowserTest/EdgeBrowserDemo.java
@@ -0,0 +1,19 @@
+package com.EdgeBrowserTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeBrowserDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.edge.driver","C:\\Users\\Sreenithi K\\Testing\\Selenium_EdgeBrowser_Launch\\EdgeBrowserJars\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.naukri.com");
+		driver.get(" https://opensource-demo.orangehrmlive.com/");
+		driver.manage().window().minimize();
+
+	}
+}
